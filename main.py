474f6997d5b06d973d159005beccd1194652e015
Samm Du <hmu@sammdu.com>
2021-04-13 03:02:37 -0400

    added index html and main py

---
+++ main.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""
+Imports the `webbrowser` module and opens the `index.html` file of the current directory
+with the default web browser, in a new tab.
+"""
+import webbrowser
+webbrowser.open_new_tab("./index.html")
