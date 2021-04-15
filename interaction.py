7543939ded43617cd73b2c355bcdca9c77b7de06
Samm Du <hmu@sammdu.com>
2021-04-14 23:00:09 -0400

    implemented various bound event functions

--- interaction.py
+++ interaction.py
@@ -25,10 +25,26 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
 from browser import document as dom
-from browser import html, alert, console
+from browser import html, DOMEvent
+
+
+# GLOBAL VARIABLES
+WINNING_STEP_LEN = 3
+PLAYER_1_PIECE = 'x'
 
 game_status_text = dom['game_status']
-board = dom['board']
+
+
+class ThemeColor:
+    """
+    selection of colors used in the UI design
+    """
+    sel: str = "#22263f"
+    unsel: str = "#7399c6"
+    blue: str = "#4c94df"
+    green: str = "#1ea382"
+    orange: str = "#f4a83e"
+    purple: str = "#6145b2"
 
 
 def draw_board(table: html.TABLE, side: int) -> None:
@@ -58,15 +74,100 @@ def draw_board(table: html.TABLE, side: int) -> None:
     """
 
 
+def switch_selection(
+    selected: html.BUTTON,
+    button_class: str,
+    colortype: str = "background-color"
+) -> None:
+    """
+    helper function to remove darkened background from all buttons in the
+    `button_class`, and darken the `selected` button
+    changes the foreground or background collor based on the `colortype` option
+    """
+
+    # clear background color for all buttons in the `button_class`
+    for b in dom.select(button_class):
+        b.attrs["style"] = f"{colortype}: {ThemeColor.unsel};"
+
+    # darken `selected` button's background color
+    selected.attrs["style"] = f"{colortype}: {ThemeColor.sel};"
+
+
+def ev_change_board_size(event: DOMEvent) -> None:
+    """
+    change the board size based on the given button event
+    """
+    target = event.target
+
+    # change the button colors to reflect user selection
+    switch_selection(target, ".btn-len")
+
+    # grab new side length and redraw board
+    new_side_len = int(target.name[-1])
+    draw_board(board, new_side_len)
+
+    # log the change in the broswer console
+    print(f"Changed board side length to: {new_side_len}")
+
+
+def ev_change_win_step(event: DOMEvent) -> None:
+    """
+    change the number of steps required to win the game based on the
+    given button event
+    """
+    target = event.target
+
+    # change the button colors to reflect user selection
+    switch_selection(target, ".btn-win")
+
+    # grab new winning length and set the global variable
+    new_win_len = int(target.name[-1])
+    global WINNING_STEP_LEN
+    WINNING_STEP_LEN = new_win_len
+
+    # log the change in the broswer console
+    print(f"Set winning step length to: {WINNING_STEP_LEN}")
+
+
+def ev_change_player1_piece(event: DOMEvent) -> None:
+    """
+    change the game piece (x/o) used by player 1 based on the given
+    button event
+    """
+    target = event.target
+
+    # change the button colors to reflect user selection
+    switch_selection(target, ".btn-piece", colortype="color")
+
+    # grab new winning length and set the global variable
+    new_player1_piece = target.name[-1]
+    global PLAYER_1_PIECE
+    PLAYER_1_PIECE = new_player1_piece
+
+    # log the change in the broswer console
+    print(f"Changed Player 1's game piece to: {PLAYER_1_PIECE}")
+
+
 if __name__ == '__main__':
+    print("https://sammdu.com")
 
     # draw a 3x3 board by default
-    draw_board(board, 3)
+    draw_board(dom['board'], 3)
 
     # indicate that the game is ready
     game_status_text.html = """
     Please select your options,
     <span style="display: inline-block;">and start the game below!</span>
     """
+
+    # bind functions to buttons
+    for b in dom.select('.btn-len'):
+        b.bind("click", ev_change_board_size)
+    for b in dom.select('.btn-win'):
+        b.bind("click", ev_change_win_step)
+    for b in dom.select('.btn-piece'):
+        b.bind("click", ev_change_player1_piece)
+    for b in dom.select('.btn-st'):
+        pass
 
     # dom.select('#game_status')[0].text = "Haha"
