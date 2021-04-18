a634471c140db9048aea1dc039f6dd1ea3c76d8a
Samm Du <hmu@sammdu.com>
2021-04-18 01:47:17 -0400

    include requirements.txt and make PORT constant changeable; already included in MarkUs

--- main.py
+++ main.py
@@ -37,5 +37,5 @@ Handler = http.server.SimpleHTTPRequestHandler
 if __name__ == '__main__':
     with socketserver.TCPServer(("", PORT), Handler) as httpd:
         print("serving at port", PORT)
-        webbrowser.open_new_tab("http://127.0.0.1:8000")
+        webbrowser.open_new_tab(f"http://127.0.0.1:{PORT}")
         httpd.serve_forever()
